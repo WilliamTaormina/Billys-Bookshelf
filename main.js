aa542e40e531490fe3fe7c6dad6ee7fe35fe3c80
williamtaormina <williamtaormina@gmail.com>
2018-05-28 13:05:16 -0700

    Inputs and Rough Formatting

---
+++ main.js
@@ -0,0 +1,35 @@
+// Book Title Name Input
+
+function bookOneTitleFunction() {
+  var x = document.getElementById("bookOneTitleTextInput").value;
+  document.getElementById("bookOneTitleTextDisplay").innerHTML = x;
+  document.getElementById("bookOneTitleTextInput").value = "";
+}
+function bookOneAuthorFunction() {
+  var y = document.getElementById("bookOneAuthorTextInput").value;
+  document.getElementById("bookOneAuthorTextDisplay").innerHTML = y;
+  document.getElementById("bookOneAuthorTextInput").value = "";
+}
+function bookTwoTitleFunction() {
+  var z = document.getElementById("bookTwoTitleTextInput").value;
+  document.getElementById("bookTwoTitleTextDisplay").innerHTML = z;
+  document.getElementById("bookTwoTitleTextInput").value = "";
+}
+
+function bookTwoAuthorFunction() {
+  var a = document.getElementById("bookTwoAuthorTextInput").value;
+  document.getElementById("bookTwoAuthorTextDisplay").innerHTML = a;
+  document.getElementById("bookTwoAuthorTextInput").value = "";
+}
+
+function bookThreeTitleFunction() {
+  var b = document.getElementById("bookThreeTitleTextInput").value;
+  document.getElementById("bookThreeTitleTextDisplay").innerHTML = b;
+  document.getElementById("bookThreeTitleTextInput").value = "";
+}
+
+function bookThreeAuthorFunction() {
+  var c = document.getElementById("bookThreeAuthorTextInput").value;
+  document.getElementById("bookThreeAuthorTextDisplay").innerHTML = c;
+  document.getElementById("bookThreeAuthorTextInput").value = "";
+}
